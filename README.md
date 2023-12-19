fcc9ef554595cdea90cca227f2dbda2add0551b2
Daniel de Vries <devries.d@gmail.com>
2023-12-19 14:24:15 -0500

    initial setup

---
+++ README.md
@@ -0,0 +1,19 @@
+# hn-expand-comment
+
+Improve the readability of deep nested comments on [Hacker News](https://news.ycombinator.com/) mobile website.
+
+This userscript solves the problem of reading comments that are nested deep and thus very narrow. There is a lot of mobile screen real estate not being used. However, not every comment that is nested needs the expand functionality, only comments nested deeper than 6 levels and have over 400 characters (differs among smartphones screen sizes).
+
+## Demo:
+
+![Demo of hn-expand-comment](/demo.gif)
+
+## Usage
+
+1. Install a userscript manager:
+- Tampermonkey [Chrome](https://chromewebstore.google.com/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo)  [Firefox](https://addons.mozilla.org/en-US/firefox/addon/tampermonkey/)
+- Violentmonkey [Chrome](https://chromewebstore.google.com/detail/violentmonkey/jinjaccalgkegednnccohejagnlnfdag)  [Firefox](https://addons.mozilla.org/en-US/firefox/addon/violentmonkey/).
+2. Install the user script from greasefork (INSTALL link).
+
+## TODO
+- When expanded, change button text to 'contract'
---
+++ hn-expand-comment.user.js
@@ -0,0 +1,58 @@
+// ==UserScript==
+// @name         HN Expand Comment
+// @namespace    https://news.ycombinator.com/item
+// @description  Expand nested comments to improve readability on HN comments 
+// @match        https://news.ycombinator.com/item*
+// @author       Daniel de Vries
+// @version      0.1
+// ==/UserScript==
+
+
+function main () {
+    const ctree = document.querySelector("table.comment-tree");
+    const tbody = ctree.querySelector("tbody");
+    const tablerows = tbody.children
+
+    for (let i = 0; i < tablerows.length; i++) {
+        var comment = tablerows[i];
+        var commentlength = comment.querySelector(".commtext").innerText.length
+        var indenttd = comment.querySelector(".ind");
+        let indent = indenttd.getAttribute("indent");
+        // only add Expand button to comments nested deeper than 6
+        // and have a comment charater length larger than 400
+        if (indent < 6 || commentlength < 400) {continue}
+        // if (indent < 6) {continue}
+        var commentbtns = comment.querySelector(".comhead")
+        var commentnav = commentbtns.querySelector(".navs")
+        let input = document.createElement("input");
+        styleBtn(input);
+        input.dataset.commentid = comment.id;
+        input.type="button";
+        input.value="expand";
+        input.onclick = function(){
+            expandComment(input.dataset.commentid)
+        };
+        commentnav.textContent += " | "
+        commentbtns.appendChild(input); 
+    }
+    function expandComment(argument){
+        var comment = document.getElementById(argument)
+        var indenttd = comment.querySelector(".ind");
+        let indent = indenttd.getAttribute("indent");
+        var gif = indenttd.querySelector("img")
+        if (gif.width == 0 && indent > 0) {
+            gif.width = 40 * indent 
+        } else {
+            gif.width = 0
+        }
+    }
+    function styleBtn(btn){
+        btn.style.background = "none";
+        btn.style.color = "inherit";
+        btn.style.border = "none";
+        btn.style.padding = "0";
+        btn.style.cursor = "pointer";
+        btn.style.font = "inherit";
+    }
+}; main()
+
